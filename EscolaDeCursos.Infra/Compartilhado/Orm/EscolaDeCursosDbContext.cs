1d7a6a3dcd1477f1288b95be750fdbf5d7e82889
Escola de Cursos <dev@escoladecursos.local>
2026-07-14 14:37:45 +0000

    feat(infra): DbSets de todas as entidades no EscolaDeCursosDbContext

--- EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
+++ EscolaDeCursos.Infra/Compartilhado/Orm/EscolaDeCursosDbContext.cs
@@ -1,4 +1,11 @@
 using System.Reflection;
+using EscolaDeCursos.Dominio.Modulos.ModuloAluno;
+using EscolaDeCursos.Dominio.Modulos.ModuloAula;
+using EscolaDeCursos.Dominio.Modulos.ModuloCategoria;
+using EscolaDeCursos.Dominio.Modulos.ModuloCurso;
+using EscolaDeCursos.Dominio.Modulos.ModuloInstrutor;
+using EscolaDeCursos.Dominio.Modulos.ModuloMatricula;
+using EscolaDeCursos.Dominio.Modulos.ModuloTurma;
 using Microsoft.EntityFrameworkCore;
 
 namespace EscolaDeCursos.Infra.Compartilhado.Orm;
@@ -6,6 +13,14 @@ namespace EscolaDeCursos.Infra.Compartilhado.Orm;
 public sealed class EscolaDeCursosDbContext(
     DbContextOptions<EscolaDeCursosDbContext> options) : DbContext(options)
 {
+    public DbSet<Categoria> Categorias => Set<Categoria>();
+    public DbSet<Curso> Cursos => Set<Curso>();
+    public DbSet<Aula> Aulas => Set<Aula>();
+    public DbSet<Instrutor> Instrutores => Set<Instrutor>();
+    public DbSet<Aluno> Alunos => Set<Aluno>();
+    public DbSet<Turma> Turmas => Set<Turma>();
+    public DbSet<Matricula> Matriculas => Set<Matricula>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         Assembly assembly = typeof(EscolaDeCursosDbContext).Assembly;
